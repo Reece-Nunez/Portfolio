c363c23a34bd6ee3acd3cd1f106c9c952e98c386
Reece Nunez <116838102+Reece-Nunez@users.noreply.github.com>
2022-11-05 21:24:27 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("#name").fadeIn(1200);
+})
+$(document).ready(function() {
+    $("#whatAmI").fadeIn(3000);
+})
+$(document).ready(function() {
+    $("#description").fadeIn(4000);
+})
+$(document).ready(function() {
+    $("#linkedin-container").fadeIn(6000).css("display","inline-flex");
+})
+$(document).ready(function() {
+    $("#email-container").fadeIn(6000).css("display","inline-flex");
+})
